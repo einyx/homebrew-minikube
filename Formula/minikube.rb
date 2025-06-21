802537a96b686b53b027b5cebb59ff71e9959fd2
alessio <alessio@linux.com>
2025-06-21 03:39:43 +0200

    Update minikube formula to use homebrew-minikube release v1.36.0-ssh

--- Formula/minikube.rb
+++ Formula/minikube.rb
@@ -8,15 +8,15 @@ class Minikube < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/einyx/minikube/releases/download/v1.36.0-ssh/minikube-darwin-arm64"
-      sha256 "08809785caefdb005078035897104e6db16709d47ca3e17a8fa8550de1338f53"
+      url "https://github.com/einyx/homebrew-minikube/releases/download/v1.36.0-ssh/minikube-darwin-arm64"
+      sha256 "428a6690733038c4f17e5ab08c44616e94bc1fffdb16149386dff792037e66eb"
 
       def install
         bin.install "minikube-darwin-arm64" => "minikube"
       end
     else
-      url "https://github.com/einyx/minikube/releases/download/v1.36.0-ssh/minikube-darwin-amd64"
-      sha256 "5a6c9ca094d129f6d31b94fd28394e40020955086f47d3de5ced18d64f38b8a2"
+      url "https://github.com/einyx/homebrew-minikube/releases/download/v1.36.0-ssh/minikube-darwin-amd64"
+      sha256 "3a69308d25e814f3da4d36fa8321bdb90e50bae17ba424ad1e9fe9713d408de6"
 
       def install
         bin.install "minikube-darwin-amd64" => "minikube"
@@ -26,15 +26,15 @@ class Minikube < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/einyx/minikube/releases/download/v1.36.0-ssh/minikube-linux-arm64"
-      sha256 "a73dd7f0f08466c090e2b8fa05e43f72e8ae19ace5a74e30bf7a9304ef7fdebf"
+      url "https://github.com/einyx/homebrew-minikube/releases/download/v1.36.0-ssh/minikube-linux-arm64"
+      sha256 "6d53cc4685e037fc70b38c326a18ad7bf7ce0e08d0be3b27b8f025652fabfc45"
 
       def install
         bin.install "minikube-linux-arm64" => "minikube"
       end
     else
-      url "https://github.com/einyx/minikube/releases/download/v1.36.0-ssh/minikube-linux-amd64"
-      sha256 "70cc2da1288a634019cfb25fe7305e3044708de955ee6c910aa83503d4c9d110"
+      url "https://github.com/einyx/homebrew-minikube/releases/download/v1.36.0-ssh/minikube-linux-amd64"
+      sha256 "6ecf4cff0d2058036177f5db3ff446649bb2435c7b9b4f530617ae1bfe5480e4"
 
       def install
         bin.install "minikube-linux-amd64" => "minikube"
