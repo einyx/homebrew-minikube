c8961c63c65e9b7ce6b3f0d1580acb362a072fe7
alessio <alessio@linux.com>
2025-06-25 21:52:57 +0200

    Fix URLs in minikube formula to point to the correct remote repository

--- Formula/minikube.rb
+++ Formula/minikube.rb
@@ -8,14 +8,14 @@ class Minikube < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/einyx/homebrew-minikube-remote/releases/download/v1.36.0-remote/minikube-darwin-arm64"
+      url "https://github.com/einyx/minikube-remote/releases/download/v1.36.0-remote/minikube-darwin-arm64"
       sha256 "428a6690733038c4f17e5ab08c44616e94bc1fffdb16149386dff792037e66eb"
 
       def install
         bin.install "minikube-darwin-arm64" => "minikube"
       end
     else
-      url "https://github.com/einyx/homebrew-minikube-remote/releases/download/v1.36.0-remote/minikube-darwin-amd64"
+      url "https://github.com/einyx/minikube-remote/releases/download/v1.36.0-remote/minikube-darwin-amd64"
       sha256 "3a69308d25e814f3da4d36fa8321bdb90e50bae17ba424ad1e9fe9713d408de6"
 
       def install
@@ -26,14 +26,14 @@ class Minikube < Formula
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/einyx/homebrew-minikube-remote/releases/download/v1.36.0-remote/minikube-linux-arm64"
+      url "https://github.com/einyx/minikube-remote/releases/download/v1.36.0-remote/minikube-linux-arm64"
       sha256 "6d53cc4685e037fc70b38c326a18ad7bf7ce0e08d0be3b27b8f025652fabfc45"
 
       def install
         bin.install "minikube-linux-arm64" => "minikube"
       end
     else
-      url "https://github.com/einyx/homebrew-minikube-remote/releases/download/v1.36.0-remote/minikube-linux-amd64"
+      url "https://github.com/einyx/minikube-remote/releases/download/v1.36.0-remote/minikube-linux-amd64"
       sha256 "6ecf4cff0d2058036177f5db3ff446649bb2435c7b9b4f530617ae1bfe5480e4"
 
       def install
